7cb4215e4f6cd34a29ae7bacc6c4ca21c4e1475e
dny8888 <dny8888@gmail.com>
2020-08-24 21:04:11 -0300

    regra de nogócios agendamentos

--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,9 @@ import routes from './routes';
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.json({ message: 'Hello World!' });
-});
+app.use(express.json());
 
+app.use(routes);
 app.listen(3333, () => {
     console.log('> Server started on port 3333');
 });
