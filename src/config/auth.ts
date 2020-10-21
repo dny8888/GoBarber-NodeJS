653e4113c80b980ef0eae6d5c7242b20455ab677
dny8888 <dny8888@gmail.com>
2020-10-21 00:06:16 -0300

    refac: change to DDD

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,6 +1,6 @@
 export default {
-    jwt: {
-        secret: 'a02905d447fd50dad1065b06b569d87d',
-        expiresIn: '1d',
-    },
+  jwt: {
+    secret: 'a02905d447fd50dad1065b06b569d87d',
+    expiresIn: '1d',
+  },
 };
---
+++ src/modules/users/infra/typeorm/entities/User.ts
@@ -0,0 +1,34 @@
+/* eslint-disable camelcase */
+import {
+  Entity,
+  Column,
+  PrimaryColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('users')
+class User {
+  @PrimaryColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  avatar: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
+
+export default User;
