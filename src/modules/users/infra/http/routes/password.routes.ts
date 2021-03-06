eb5b5e17206e07d9e0ca2ea288339304199ebac3
dny8888 <dny8888@gmail.com>
2021-03-05 21:59:56 -0300

    feat: add validations with celebrate

--- src/modules/users/infra/http/routes/password.routes.ts
+++ src/modules/users/infra/http/routes/password.routes.ts
@@ -21,7 +21,9 @@ passwordRouter.post(
   '/reset',
   celebrate({
     [Segments.BODY]: {
-      email: Joi.string().email().required(),
+      email: Joi.string().uuid().required(),
+      password: Joi.string().required(),
+      password_confirmation: Joi.string().required().valid(Joi.ref('password')),
     },
   }),
   resetPasswordController.create,
